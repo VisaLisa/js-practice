61cfbec24764ffa997db45b20034b2289e4ec19e
Lisa Jiamsirioungkul <visakhalisajiam@gmail.com>
2020-07-13 13:44:57 -0500

    first commit

---
+++ practice.js
@@ -0,0 +1,19 @@
+const minAge = 21;
+let yourAge = 19;
+
+'You must be ${minAge} or order to enter. Come back in ${minAge - yourAge} years'
+
+// generating a number between 1 and 10
+const step1 = Math.random();
+const step2 = step1 * 10;
+const step3 = Math.floor(step2);
+const step4 = step3 + 1;
+
+Math.floor(Math.random() * 10) + 1;
+
+alert('Hello from JS!');
+
+"hi".toUpperCase();
+
+console.log("hi".toUpperCase());
+console.log(3 + 4);

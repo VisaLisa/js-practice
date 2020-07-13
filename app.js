fe9b3ec52d24f84f0c88bcb64f74474e3ebe4b16
Lisa Jiamsirioungkul <visakhalisajiam@gmail.com>
2020-07-13 13:58:37 -0500

    else if statements

---
+++ app.js
@@ -0,0 +1,23 @@
+//example 1
+
+if (1 === 1) {
+    console.log("It's True!");
+}
+
+let rating = 'ten';
+
+if (rating === 3){
+    console.log('You are a super star!');
+}
+else if (rating === 2) {
+    console.log ('Meets expectations');
+}
+else if (rating === 1) {
+    console.log ('Needs improvement');
+}
+else {
+    console.log ("Invalid rating")
+}
+
+//example 2
+

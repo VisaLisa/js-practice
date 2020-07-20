ec68743a92e98308eb310be78e3bfcead45ca48c
Lisa Jiamsirioungkul <visakhalisajiam@gmail.com>
2020-07-19 22:44:43 -0500

    loops corrections

--- app.js
+++ app.js
@@ -196,20 +196,59 @@ people.splice(1,0,'Bob');
 
 // OBJECT SECTION
 
-const Lisa (
-    height: 62;
-    weight: 150; 
-    city: 'Seattle';
-    isAdmin: true;
-    age: 29; 
-    grade: {
-        midterm: 80;
-        final: 100;
-    }
-)
-
-//loops
-for(let i =1; i <= 10; i += 3){
-    console.log('Hello:', i);
+// const Lisa = [
+//     height: 62;
+//     weight: 150; 
+//     city: 'Seattle';
+//     age: 29; 
+//     grade: [
+//         midterm: 80;
+//         final: 100;
+//     ]
+// ];
+// loops
+for (let bet = 1; bet <= 10; bet += 3){
+    console.log('Hello');
 }
 
+// for(let num = 1; num <= 20; num++){
+//     console.log(`${num}x${num} = ${num * num}`);
+// }
+
+const myStudents = [
+	{
+		firstName : 'Zeus',
+		grade     : 86
+	},
+	{
+		firstName : 'Artemis',
+		grade     : 97
+	},
+	{
+		firstName : 'Hera',
+		grade     : 72
+	},
+	{
+		firstName : 'Apollo',
+		grade     : 90
+	}
+];
+
+for (let i = 0; i < myStudents.length; i++) {
+	let student = myStudents[i];
+	console.log(`${student.firstName} scored ${student.grade}`);
+}
+
+// let total = 0;
+
+// for (let i = 0; i < myStudents.length; i++){
+//     let student = myStudents[i];
+//     total += student.grade;
+// }
+// console.log(total / myStudents.length);
+
+const examScores = [98, 77, 84, 91, 57, 66];
+
+for (let i = 0; i <= examScores.length; i++){
+    console.log(i, examScores[i]);
+};

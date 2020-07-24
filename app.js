a64d1e675bbe523afe010d824f3c21e165c12e56
Lisa Jiamsirioungkul <visakhalisajiam@gmail.com>
2020-07-24 01:10:14 -0500

    get cards

--- app.js
+++ app.js
@@ -362,15 +362,28 @@ function isPangram(sentence){
 //     return {value, suit};
 // }
 function pick(arr) {
-    const idx = Math.floor(Math.random() * arr.length);
-    return arr(idx);
+	//return random element from arr
+	const idx = Math.floor(Math.random() * arr.length);
+	return arr[idx];
 }
 
 function getCard() {
-    const values =['1','2','3','4','5','6','7','8','9','10','J','Q','K','A'];
-    const value = pick(values);
-    const suits = ['clubs', 'spades', 'hearts', 'diamonds'];
-    const suit = pick(suits);
-    return { value : value, suit: suit };
+	const values = [
+		'1',
+		'2',
+		'3',
+		'4',
+		'5',
+		'6',
+		'7',
+		'8',
+		'9',
+		'10',
+		'J',
+		'Q',
+		'K',
+		'A'
+	];
+	const suits = [ 'clubs', 'spades', 'hearts', 'diamonds' ];
+	return { value: pick(values), suit: pick(suits) };
 }
-
